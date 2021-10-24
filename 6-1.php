5ec68dfdd436ffc846b84e9549e57b8a7e982f45
kamezokogoku <81956061+kamezokogoku@users.noreply.github.com>
2021-10-24 16:31:34 +0300

    Update 6-1
    Исправил на русский ошибку при чтение. Добавил проверку на существование и доступность файла

--- 6-1.php
+++ 6-1.php
@@ -1,20 +1,21 @@
 <?php
-function writelog($value){
-    $fileName = "log_" . date("Y-m-d");
-    $file = fopen($fileName . ".txt", "a+");
-    $writeToFile= "### " . date("d.m.Y H:i:s") . "\n" . $value . "\n\n\n";
-
-    if (!$file){
-        echo "Error";
+function writelog($value) {
+    $fileName = "log_" . date("Y-m-d") . ".txt";
+    if (is_readable($fileName) || file_exists($fileName)){
+        $file = fopen($fileName , "a+");
+        $writeToFile= "### " . date("d.m.Y H:i:s") . "\n" . $value . "\n\n\n";
     }
     else {
-        if (filesize($fileName . "txt") > 100);
-        {
-            fclose($file);
-            $fileName = "log_" . date("Y-m-d");
-            $file = fopen($fileName . ".txt", "a+");
-        }
+        echo "Ошибка чтения";
     }
+
+    if (filesize($fileName . "txt") > 100);
+    {
+        fclose($file);
+        $fileName = "log_" . date("Y-m-d");
+        $file = fopen($fileName . ".txt", "a+");
+    }
+
     fwrite($file, $writeToFile);
     fclose($file);
 }
