2142ba72ee38069007eea15be1ccffdd2cf7e5a2
kamezokogoku <81956061+kamezokogoku@users.noreply.github.com>
2021-11-02 11:06:18 +0300

    Update CalculatorLogic

--- Calculator/CalсulatorLogic.php
+++ Calculator/CalсulatorLogic.php
@@ -1,35 +1,34 @@
 <?php
-if (empty($_GET)) {
-    return 'Ничего не передано!'; // Проверка переданы ли переменные
-}
-if (empty($_GET['operation'])){
-    return 'Не передана операция';
-}
-if (empty($_GET['n1']) || empty($_GET['n2'])){
-    return 'Не переданы переменные';
-}
-if ($_GET['n2'] == 0){
-    return 'на 0 делить нельзя!'; // Если ввести 0 то сервер считает это как отсутствие данных а не как цифра 0
-}
 $n1 = $_GET['n1'];
 $n2 = $_GET['n2'];
 
-$expression = $n1 . ' ' . $_GET['operation'] . ' ' . $n2 . ' = ';
-switch ($_GET['operation']){
-    case '+';
-    $result = $n1 + $n2;
-        break;
-    case '-';
-    $result = $n1 - $n2;
-        break;
-    case '*';
-    $result = $n1 * $n2;
-        break;
-    case '/';
-    $result = $n1 / $n2;
-        break;
-    case ' ';
-        return 'Операция не поддерживается';
+if ((!$n1 && $n1 !='0') || (!$n2 && $n2 != '0')) {
+    return 'Не переданы переменные.'; // Проверка заполнены ли поля.
 }
-return $expression . $result;
+if(!is_numeric($n1) || !is_numeric($n2)){
+    return 'Данные должны быть числами.'; // Выдавать ошибку если пользователь вводит букв вместо цифр.
+}
+    else {
+        $expression = $n1 . ' ' . $_GET['operation'] . ' ' . $n2 . ' = '; // функция вывода получаемое выражение
+        switch ($_GET['operation']) {
+            case '+';
+                $result = $n1 + $n2;
+                break;
+            case '-';
+                $result = $n1 - $n2;
+                break;
+            case '*';
+                $result = $n1 * $n2;
+                break;
+            case '/':
+                if($n2 == '0'){
+                    $result = 'На 0 делить нельзя!'; // условие когда происходит деление на 0
+                }
+                else{
+                    $result = $n1 / $n2;
+                }
+                break;
+        }
+        return $expression . $result;
+    }
 
